b53b0d6c85603e36c819bc7691dad3c03b16bd6d
gloriaadanna <gloriaagwam7@gmail.com>
2024-05-27 14:36:37 +0100

    Build app middleware pipeline

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Infrastructure;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+builder.Services.AddDatabaseContext(configuration);
+builder.Services.AddHttpContextAccessor();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -12,6 +22,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.MigrateDatabase();
 }
 
 app.UseHttpsRedirection();
+
+app.Run();
+
+public static class ConfigurationExtension
+{
+    public static void MigrateDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        context.Database.Migrate();
+    }
+}
